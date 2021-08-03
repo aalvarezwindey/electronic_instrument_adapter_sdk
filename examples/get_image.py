c4e6e7052527b88437c0819e2cd593630c2f0855
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2021-08-02 23:03:18 -0300

    manual

---
+++ examples/get_image.py
@@ -0,0 +1,27 @@
+import electronic_instrument_adapter_sdk
+import argparse
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--host", type=str, help="server host", default="127.0.0.1")
+  parser.add_argument("--port", type=int, help="server port", default=8080)
+  args = parser.parse_args()
+
+  sdk = electronic_instrument_adapter_sdk.EIA(args.host, args.port)
+
+  instruments = sdk.list_instruments()
+  cammera = None
+  for i in instruments:
+    if i.ID == "CAM_ID":
+      cammera = i
+
+  if cammera:
+    result = cammera.send("get_image", "bytes")
+    print("Saving image bytes...")
+    with open("image.jpeg", "wb") as f:
+      f.write(result)
+  else:
+    print("Cammera with ID 'CAM_ID' not found")
+
+if __name__ == "__main__":
+    main()
