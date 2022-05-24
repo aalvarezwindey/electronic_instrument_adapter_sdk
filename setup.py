094302d2b4c9c312675e6548ff1a61f94f7731d0
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-05-24 20:39:16 -0300

    preparing for version 0.2.5

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 
 from setuptools import setup, find_packages
 
+VERSION_PLACEHOLDER = '0.2.5'
+
 setup(
-  name = 'electronic_instrument_adapter_sdk',         # How you named your package folder (MyLib)
+  name = 'Open_LISA_SDK',         # How you named your package folder (MyLib)
   packages=find_packages(),
-  version = '0.2.4',      # Start with a small number and increase it with every change you make
+  version = VERSION_PLACEHOLDER,      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'SDK for Electronic Instrument Adapter',   # Give a short description about your library
+  description = 'SDK for Laboratory Instrument Station Adapter',   # Give a short description about your library
   author = 'Ariel Alvarez Windey & Gabriel Robles',                   # Type in your name
   author_email = 'ajalvarez@fi.uba.ar',      # Type in your E-Mail
   url = 'https://github.com/aalvarezwindey/Open-LISA-SDK',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/aalvarezwindey/Open-LISA-SDK/archive/refs/tags/0.2.4.tar.gz',
-  keywords = ['SDK', 'ELECTRONIC', 'INSTRUMENT', 'ADAPTER', 'FIUBA'],   # Keywords that define your package best
+  download_url = 'https://github.com/aalvarezwindey/Open-LISA-SDK/archive/refs/tags/{}.tar.gz'.format(VERSION_PLACEHOLDER),
+  keywords = ['SDK', 'ELECTRONIC', 'INSTRUMENT', 'ADAPTER', 'FIUBA', 'OPEN', 'LISA', 'LABORATORY'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
