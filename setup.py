7acdcbd3811b789688570f7aa036050a1f3bb846
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-05-24 21:03:15 -0300

    preparing for version 0.2.7

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION_PLACEHOLDER = '0.2.6'
+VERSION_PLACEHOLDER = '0.2.7'
 
 setup(
   name = 'Open_LISA_SDK',         # How you named your package folder (MyLib)
