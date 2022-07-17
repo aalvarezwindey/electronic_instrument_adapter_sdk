aae819c6c8c772e2ff6c7538e647b32945be79ca
Gabriel Robles <grobles@fi.uba.ar>
2022-07-18 01:31:01 +0200

    minor fix on getfile

--- Open_LISA_SDK/__init__.py
+++ Open_LISA_SDK/__init__.py
@@ -197,7 +197,7 @@ class SDK:
             return self._client_protocol.send_file(data, file_target_name)
 
     def get_file(self, remote_file_name, file_target_name):
-        file_bytes = self._client_protocol.get_file(remote_file_name, file_target_name)
+        file_bytes = self._client_protocol.get_file(remote_file_name)
         with open(file_target_name, "wb") as file:
             file.write(file_bytes)
 
