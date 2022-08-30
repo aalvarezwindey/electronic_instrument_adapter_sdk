ba20f9cb6a1c01f8ab7392dcf30906d361c16dd5
Ariel Alvarez Windey <ariel.alvarez@pedidosya.com>
2022-08-29 23:44:10 -0300

    preparing for version 0.9.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from setuptools import setup, find_packages
 
-VERSION_PLACEHOLDER = '0.8.3'
+VERSION_PLACEHOLDER = '0.9.0'
 
 setup(
   name = 'Open_LISA_SDK',         # How you named your package folder (MyLib)
